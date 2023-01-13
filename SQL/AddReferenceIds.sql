cf26697d318d5e0b31a03205ef56c0c7dd626a83
Brian <59103558+brian-p28@users.noreply.github.com>
2023-01-13 10:49:35 -0800

    Add files via upload

---
+++ SQL/AddReferenceIds.sql
@@ -0,0 +1,10 @@
+USE [Migrately]
+GO
+
+/****** Object:  UserDefinedTableType [dbo].[AddReferenceIds]    Script Date: 1/13/2023 10:37:34 AM ******/
+CREATE TYPE [dbo].[AddReferenceIds] AS TABLE(
+	[Id] [int] NOT NULL
+)
+GO
+
+
---
+++ SQL/UserRoles_BatchInsert.sql
@@ -0,0 +1,10 @@
+USE [Migrately]
+GO
+
+/****** Object:  UserDefinedTableType [dbo].[UserRoles_BatchInsert]    Script Date: 1/13/2023 10:37:24 AM ******/
+CREATE TYPE [dbo].[UserRoles_BatchInsert] AS TABLE(
+	[Id] [int] NOT NULL
+)
+GO
+
+
